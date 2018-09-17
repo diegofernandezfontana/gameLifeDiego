665093ca61837a8adf88ca1b818b84305532657e
diegofernandezfontana <diegofernandezfontana@gmail.com>
2018-09-17 11:37:40 -0300

    Limpiando un poco el codigo misma funcionalidad

--- game.js
+++ game.js
@@ -1,10 +1,8 @@
 var gameOfLife = {
 
-    width: 12,
-    height: 12, // dimensiones alto y ancho del tablero
-    stepInterval: null, // debería ser usada para guardar referencia a una intervalo que esta siendo jugado
-    //var botonClear = document.querySelector("#clear_btn");
-//    var botonClear = document.querySelector("#clear_btn");
+    width: 50,
+    height: 50, // dimensiones alto y ancho del tablero
+    stepInterval: null,
     createAndShowBoard: function () {
 
         // crea el elemento <table>
@@ -17,7 +15,6 @@ var gameOfLife = {
         botonResetRandom.addEventListener('click', this.resetRandom);
         var botonPlay = document.querySelector("#play_btn");
         botonPlay.addEventListener('click', this.enableAutoPlay);
-        //goltable crea la tbody dentro de table#board
 
         // Construye la Tabla HTML
         var tablehtml = '';
@@ -31,7 +28,6 @@ var gameOfLife = {
         //Se le asigna tablehtml a goltable
         goltable.innerHTML = tablehtml;
 
-        // agrega la tabla a #board
         var board = document.getElementById('board');
         board.appendChild(goltable);
         // una vez que los elementos html son añadidos a la pagina le añadimos los eventos
@@ -39,13 +35,6 @@ var gameOfLife = {
     },
 
     forEachCell: function (iteratorFunc) {
-        /*
-          Escribe forEachCell aquí. Vas a necesitar visitar
-          cada celda en el tablero, llama la "iteratorFunc",
-          y pasa a la funcion, la celda y la coordenadas x & y
-          de la celda. Por ejemplo: iteratorFunc(cell, x, y)
-        */
-
         for(var x =0; x < this.width; x++){
             for(var y =0; y < this.height; y++){
                 var celda = document.getElementById(x + '-' + y);
@@ -55,36 +44,14 @@ var gameOfLife = {
     },
 
     setupBoardEvents: function() {
-        // cada celda del tablero tiene un id CSS en el formato "x-y"
-        // donde x es la coordinada-x e y es la coordenada-y
-        // usa este hecho para loopear a traves de todos los ids y asignales
-
-        // "click" events que permite a un usuario clickear en
-        // celdas para configurar el estado inicial del juego
-        // antes de clickear  "Step" o "Auto-Play"
-
-        // clickear en una celda deberia alternar la celda entre "alive" y "dead"
-        // por ejemplo: una celda "alive"  este pintado de azul, y una celda "dead" puede mantenerse blanco
-
-        // EJEMPLO PARA UNA CELDA
-        // Aquí esta como tendríamos un click event en sol una celda 0-0
-        // Necesitas agregar el click event en cada celda en el tablero
         var onCellClick = function (e) {
-            //e -> la celda clickeada
-            // Pregunta para hacerte a ti mismo: Que es this en este contexto?
-
-            // como setear el estilo de la celda cuando es clickeada
             if (this.dataset.status == 'dead') {
-                  //This hacer referencia a la celda clickeada
-                  //Si la celda esta muerta, la activa
                 this.className = 'alive';
                 this.dataset.status = 'alive';
             } else {
-                  //Si la celda esta viva, la mata
                 this.className = 'dead';
                 this.dataset.status = 'dead';
             }
-
         };
         this.forEachCell(function(cell){
             cell.addEventListener('click', onCellClick)
@@ -111,15 +78,6 @@ var gameOfLife = {
     },
 
     step: function () {
-
-        // Acá es donde querés loopear a través de las celdas
-        // en el tablero y determina, basado en tus vecinos,
-        // si la celda debe estar viva o muerta en la siguiente
-        // evolución del juego.tbody.children[y+i].children[x+j].className == "alive"
-
-        // Necesitas:
-        // 1. Cuenta vecinos vivos para todas las celdas
-        // 2. Sete el siguiente estado de todas las celdas basado en las vecinas vivas
         var tbody = document.querySelector("tbody");
         var arregloNextGen = [];
         gameOfLife.forEachCell(function(cell){
@@ -127,8 +85,6 @@ var gameOfLife = {
             var [y,x] = cell.id.split('-');
             y = parseInt(y);
             x = parseInt(x);
-            //console.log(tbody.children[0].children[0]);
-            console.log('------');
             for(var i = -1; i < 2; i++){
                 for(var j = -1; j < 2; j++){
                     if(tbody.children[y + i]){
@@ -166,7 +122,7 @@ var gameOfLife = {
     enableAutoPlay: function () {
         // Comienza Auto-Play corriendo la función step
         // automaticamente de forma reptida cada intervalo de tiempo fijo
-        setInterval(gameOfLife.step, 300);
+        setInterval(gameOfLife.step, 1000);
     }
 
 
